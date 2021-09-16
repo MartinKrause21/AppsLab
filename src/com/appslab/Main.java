6c519f130128efd8817da5383c6282549bc5b8be
z19ista13 <martin.21.krause@gmail.com>
2021-09-16 08:32:13 +0200

    snail goes up

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,21 @@
-package com.appslab;
+package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+
+        double towerHeight = 100;
+        double stairHeight = 20;
+        double stairLenght = 5;
+
+        double result;
+
+        result = (towerHeight/stairHeight)*(stairHeight+stairLenght);
+
+        System.out.println(result);
+
+
+
+
     }
 }
