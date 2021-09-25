01cfec6a29fdab722cdb42abcee3f3f9bf4b7af6
MartinKrause21 <martin.21.krause@gmail.com>
2021-09-25 09:51:58 +0200

    multiply number by array length

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -6,26 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int[] array = { 2, 3, 4, 5};
 
-        System.out.println("Zadaj prve cislo: ");
-        int a = sc.nextInt() ;
+        for (int i = 0; i < array.length; i++) {
 
-
-        System.out.println("Zadaj druhe cislo: ");
-        int b = sc.nextInt();
-
-
-        System.out.println("Zadaj tretie cislo: ");
-        int c = sc.nextInt() ;
-
-         if (a + b > c && b + c > a && a + c > b ) {
-            System.out.print("true");
+            int a = array.length;
+            System.out.println(array[i] * a);
         }
-
-        else {
-            System.out.print("false");
-        }
-
     }
 }
