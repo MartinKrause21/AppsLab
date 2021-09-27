0daa0915a34406fe567498ba5fc8a01c3011079a
MartinKrause21 <martin.21.krause@gmail.com>
2021-09-27 18:58:30 +0200

    Check max and min numbers in array and get the difference between them.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,17 +1,19 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = { 2, 3, 4, 5};
+        int[] array = { 44, 32, 86, 19, 60, 14};
 
-        for (int i = 0; i < array.length; i++) {
 
-            int a = array.length;
-            System.out.println(array[i] * a);
+        int max = Arrays.stream(array).max().getAsInt();
+        int min = Arrays.stream(array).min().getAsInt();
+
+        System.out.printf("Rozdiel cisel je: ");
+            System.out.println(max - min);
         }
     }
-}
