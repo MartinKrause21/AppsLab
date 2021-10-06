8e8e576cb45630b430b0843ec8d39504f944bc93
MartinKrause21 <martin.21.krause@gmail.com>
2021-10-06 20:06:40 +0200

    snail goes up

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,31 +1,12 @@
 package com.company;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-
-        String[] rook1 = {"A8", "E8"};
-        String[] rook2 = {"B6", "A1"};
-        String[] rook3 = {"H3", "H6"};
-
-        System.out.print(RookAttack(rook1) + "\n");
-        System.out.print(RookAttack(rook2) + "\n");
-        System.out.print(RookAttack(rook3));
-
+    public static void main(String[] args)
+    {
+        totalDistance(0.3, 0.2, 25.0);
     }
-
-     static boolean RookAttack(String[] rook) {
-        String rook1 = rook[0];
-        String rook2 = rook[1];
-
-        if (rook1.charAt(0) == rook2.charAt(0) || rook1.charAt(1) == rook2.charAt(1))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    private static void totalDistance(double heightOfStep, double lenghtOfStep, double towerHeight)
+    {
+        System.out.println(((towerHeight/heightOfStep)*lenghtOfStep)+towerHeight);
     }
 }
